bf9ea8734adfe981109db8a05185c944bcd7c221
RyanNgCT <48358569+RyanNgCT@users.noreply.github.com>
2024-05-24 23:44:40 +0800

    v0.2 - add hashing feature, some error handling

--- downloadPDFs.py
+++ downloadPDFs.py
@@ -1,6 +1,7 @@
 import requests
 import os, sys
 from dotenv import load_dotenv
+import hashlib
 
 def getVars() -> tuple[str, str]:
     """
@@ -10,6 +11,7 @@ def getVars() -> tuple[str, str]:
     cookie = os.getenv('COOKIE')
 
     return base_url, cookie
+
 
 def recursivePDFDownload(base_url: str, cookie: str, naming: str, start: int, stop: int):
     """
@@ -32,6 +34,9 @@ def recursivePDFDownload(base_url: str, cookie: str, naming: str, start: int, st
     # Create the directory if it doesn't exist
     os.makedirs(download_dir, exist_ok=True)
 
+    status_dict = {}
+    success_count, fail_count = 0, 0
+
     for i in range(start, stop + 1):
         file_index = f'{i:02}' # might need to modify accordingly
 
@@ -41,10 +46,28 @@ def recursivePDFDownload(base_url: str, cookie: str, naming: str, start: int, st
 
             file_path = os.path.join(download_dir, f'{naming}{file_index}.pdf')
             with open(file_path, 'wb') as file:
-                file.write(response.content)
-            print(f'{file_path} downloaded successfully')
+                hasher = hashlib.sha256()
+                for chunk in response.iter_content(chunk_size=4096):
+                    file.write(chunk)
+                    hasher.update(chunk)
+                file_hash = hasher.hexdigest()
         except requests.exceptions.RequestException as e:
-            print(f'Failed to download file {naming}{file_index}.pdf. Error: {e}')
+            print(f'\nFailed to download file {naming}{file_index}.pdf. Error: {e}')
+            fail_count += 1
+        else:
+            line_to_print = f'{naming}{file_index} downloaded successfully. SHA256: {file_hash}'
+            print(line_to_print)
+            status_dict[i] = line_to_print
+            success_count += 1
+
+    with open('out.log', 'w') as f:
+        f.write(f'{success_count} files successfully downloaded.\n')
+        if fail_count > 0:
+            f.write(f'{fail_count} files not downloaded!\n')
+        f.write('=================================\n\n')
+        for key, val in status_dict.items():
+            f.write(f'[{key}] {val}\n')
+        f.write('\nOperation Complete.')
 
 
 def testConnection(base_url: str, cookie: str, start: int) -> bool:
@@ -79,9 +102,20 @@ def testConnection(base_url: str, cookie: str, start: int) -> bool:
             print('An error occurred trying to connect to the server')
             return False
         
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
         print(f'An exception occurred: {e}')
         raise
+
+def hashFile(file_path: str):
+    hasher = hashlib.sha256()
+    with open(file_path, 'rb') as file:
+        chunk_size = 4096
+        while True:
+            chunk = file.read(chunk_size)
+            if not chunk:
+                break
+            hasher.update(chunk)
+    return hasher.hexdigest()
 
 
 if __name__ == '__main__':
@@ -89,6 +123,8 @@ if __name__ == '__main__':
     try:
         indices = input("Enter start and stop indices to download (inclusive): ")
         start, stop = map(int, indices.split(','))  # Split input by comma and convert to ints
+        if start < 1:
+            raise ValueError
     except ValueError:
         sys.exit("Invalid input. Run the program again!")
     else:
